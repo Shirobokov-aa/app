80c18520ac64fb60649587c5c09aac8e2f600dd4
Shirobokov-aa <136621836+Shirobokov-aa@users.noreply.github.com>
2023-10-25 20:45:02 +0300

    дроп

--- js/main.js
+++ js/main.js
@@ -1,38 +1,97 @@
 document.addEventListener("DOMContentLoaded", () => {
-    let switchButtons = document.querySelectorAll(".swicth__item")
-    console.log(switchButtons);
 
-    switchButtons.forEach(switchButton =>{
-    switchButton.addEventListener("click", (e) => {
-        e.preventDefault();
-        console.log(switchButton.dataset.value)
-        setEvent(switchButton.dataset.value)
-    })
-    })
 
-    function setEvent(curEvent){
-        switchButtons.forEach(switchButton => {
-            switchButton.classList.remove("swicth__item--js-active");
-        })
-        let tabContents = document.querySelectorAll(".content");
-        tabContents.forEach (tabContent =>{
-            tabContent.classList.remove("js-active")
-        })
-        let switchButtonActive = document.querySelector(`[data-value="${curEvent}"]`)
-        switchButtonActive.classList.add("swicth__item--js-active");
 
-        let tabContentActive = document.querySelector(`[data-event="${curEvent}"]`)
-        tabContentActive.classList.add("js-active");
+
+
+    let xml = new XMLHttpRequest();
+    console.log(xml);
+
+    xml.open('GET', 'test.txt')
+    xml.onreadystatechange = function(){
+        console.log(xml.readyState);
+        if(xml.readyState ===4){
+            alert(xml.response);
+        }
     }
+    xml.send();
 
-    var swiper = new Swiper(".photos__slider", {
-      slidesPerView: 4,
-      spaceBetween: 30,
-      navigation: {
-        nextEl: ".photos__arrow--right",
-        prevEl: ".photos__arrow--left",
-      }
-    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // let switchButtons = document.querySelectorAll(".swicth__item")
+    // console.log(switchButtons);
+
+    // switchButtons.forEach(switchButton =>{
+    // switchButton.addEventListener("click", (e) => {
+    //     e.preventDefault();
+    //     console.log(switchButton.dataset.value)
+    //     setEvent(switchButton.dataset.value)
+    // })
+    // })
+
+    // function setEvent(curEvent){
+    //     switchButtons.forEach(switchButton => {
+    //         switchButton.classList.remove("swicth__item--js-active");
+    //     })
+    //     let tabContents = document.querySelectorAll(".content");
+    //     tabContents.forEach (tabContent =>{
+    //         tabContent.classList.remove("js-active")
+    //     })
+    //     let switchButtonActive = document.querySelector(`[data-value="${curEvent}"]`)
+    //     switchButtonActive.classList.add("swicth__item--js-active");
+
+    //     let tabContentActive = document.querySelector(`[data-event="${curEvent}"]`)
+    //     tabContentActive.classList.add("js-active");
+    // }
+
+    // var swiper = new Swiper(".photos__slider", {
+    //   slidesPerView: 4,
+    //   spaceBetween: 30,
+    //   navigation: {
+    //     nextEl: ".photos__arrow--right",
+    //     prevEl: ".photos__arrow--left",
+    //   }
+    // });
 })
-
 
